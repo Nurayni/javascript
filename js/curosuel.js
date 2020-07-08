d75ea13f9d06459b8478fd772dbc7325329045e2
Nurayni Mou <64106965+Nurayni@users.noreply.github.com>
2020-07-08 13:37:17 +0600

    some javascript works

---
+++ js/curosuel.js
@@ -0,0 +1,8 @@
+const track = document.querySelector('.carosuel_track');
+const slides = Array.form(track.children);
+
+
+//when i click left,move slides to the left
+//when i click right,move slides to the right
+//when i click nav indicators,move to that slides
+
